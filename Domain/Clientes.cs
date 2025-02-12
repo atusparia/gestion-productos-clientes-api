32acac94be0bb55cfe030e9f379e33dd26de33b2
Ronald Zenón Cuevas Machaca <rzcuevasm@gmail.com>
2025-02-11 22:20:22 -0500

    Agregar archivos de proyecto.

---
+++ Domain/Clientes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Clientes
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(255)]        
+        public string Correo { get; set; } 
+        [StringLength(50)]
+        public string? Telefono { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+        public bool Activo { get; set; } = true;
+
+    }
+}
---
+++ Domain/Productos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Productos
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Nombre { get; set; }
+        [Required]
+        [Column(TypeName ="decimal(10,2)")]
+        public decimal Precio { get; set; }
+        [Required]
+        public int Stock {  get; set; }
+        //Consideraciones para distintas zonas horarias y el manejo por parte del SQL
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;        
+        public bool Activo { get; set; }=true;
+    }
+}
