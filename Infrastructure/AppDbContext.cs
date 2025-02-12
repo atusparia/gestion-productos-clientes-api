801e5dc6bbf685b623aab6486b624948d4f4928c
Ronald Zenón Cuevas Machaca <rzcuevasm@gmail.com>
2025-02-11 23:04:47 -0500

    Diseño del BackEnd

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Infrastructure
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Clientes> clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source = SRVMIO;" +
+                    "initial catalog=ProCliDB; User Id=usMio; Pwd=Rcm123456;" +
+                    "TrustServerCertificate=true");
+        }
+    }
+}
